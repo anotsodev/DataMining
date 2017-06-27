d50d886b88ee9531a80b4519d84102d6055d4c11
Kyle Halog <aoschleger@gmail.com>
2017-06-27 22:20:48 +0800

    Removed unused import

--- src/datamining/DataMining.java
+++ src/datamining/DataMining.java
@@ -16,12 +16,8 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
